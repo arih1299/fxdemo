f2c234c57fc6f2e5fde7c3667291ca30b35f5c92
arih <ari@example.com>
2019-11-17 23:56:09 +0800

    initial

---
+++ web/fxdemo.js
@@ -0,0 +1,134 @@
+
+var hostname = "sgdemo1.solace.com"
+// ,sgdemo2.solace.com"
+var port = "8000"
+var user = "default"
+var pass = "default"
+var topic = "fxrates/#"
+var path = '/ws'
+var tls = false;
+
+var qos = 1;
+var retain = false;
+var timeout = 60;
+var keepAlive = 120;
+var cleanSession = true;
+
+var clientId = "" + (Math.random() + 1).toString(36).substring(2, 10);  // 8 "random" [0-9a-z]
+var connected = false;
+
+
+// Entry
+$(document).ready( function () {
+    $('#fxrate-table').DataTable({
+        "paging":   false,
+        "info":     false,
+        "searching":   false
+    });
+    
+    // Initiate Solace connection
+    connect();
+} );
+
+
+
+
+// called when the client connects
+function onConnect(context) {
+  // Once a connection has been made, make a subscription and send a message.
+  var connectionString = context.invocationContext.host + ":" + context.invocationContext.port + context.invocationContext.path;
+  logMessage("INFO", "Connection Success ", "[URI: ", connectionString, ", ID: ", context.invocationContext.clientId, "]");
+  var statusSpan = document.getElementById("connectionStatus");
+  statusSpan.innerHTML = "Connected to Solace PubSub+: " + connectionString + " with Client ID: " + context.invocationContext.clientId;
+  connected = true;
+
+  client.subscribe(topic);
+  logMessage("INFO", "Subscribed to " + topic);
+}
+
+
+function onConnected(reconnect, uri) {
+  // Once a connection has been made, make a subscription and send a message.
+  logMessage("INFO", "Client Has now connected: [Reconnected: ", reconnect, ", URI: ", uri, "]");
+  connected = true;
+}
+
+function onFail(context) {
+  logMessage("ERROR", "Failed to connect. [Error Message: ", context.errorMessage, "]");
+  var statusSpan = document.getElementById("connectionStatus");
+  statusSpan.innerHTML = "Failed to connect: " + context.errorMessage;
+  connected = false;
+}
+
+// called when the client loses its connection
+function onConnectionLost(responseObject) {
+  if (responseObject.errorCode !== 0) {
+    logMessage("INFO", "Connection Lost. [Error Message: ", responseObject.errorMessage, "]");
+  }
+  connected = false;
+}
+
+// called when a message arrives
+function onMessageArrived(message) {
+
+    var payload = JSON.parse(message.payloadString);
+    logMessage("INFO", "TOPIC='" + message.destinationName + "',&nbsp;&nbsp;&nbsp;PAYLOAD='" + message.payloadString + "'");
+    var table = document.getElementById("fxrate-table").getElementsByTagName("tbody")[0];
+    var row = table.insertRow(0);
+    row.insertCell(0).innerHTML = new Date();
+    row.insertCell(1).innerHTML = payload;
+
+}
+
+function logMessage(type, ...content) {
+  if (type === "INFO") {
+    console.info(...content);
+  } else if (type === "ERROR") {
+    console.error(...content);
+  } else {
+    console.log(...content);
+  }
+}
+
+function connect() {
+
+  client = new Paho.MQTT.Client(hostname, Number(port), path, clientId);
+  logMessage("INFO", "Connecting to Server: [Host: ", hostname, ", Port: ", port, ", Path: ", client.path, ", ID: ", clientId, "]");
+
+  // set callback handlers
+  client.onConnectionLost = onConnectionLost;
+  client.onMessageArrived = onMessageArrived;
+  client.onConnected = onConnected;
+
+  var options = {
+    invocationContext: { host: hostname, port: port, path: client.path, clientId: clientId },
+    timeout: timeout,
+    keepAliveInterval: keepAlive,
+    cleanSession: cleanSession,
+    useSSL: tls,
+    onSuccess: onConnect,
+    onFailure: onFail
+  };
+
+  if (user.length > 0) {
+    options.userName = user;
+  }
+
+  if (pass.length > 0) {
+    options.password = pass;
+  }
+
+  // connect the client
+  client.connect(options);
+  var statusSpan = document.getElementById("connectionStatus");
+  statusSpan.innerHTML = "Connecting...";
+}
+
+function disconnect() {
+  logMessage("INFO", "Disconnecting from Server.");
+  client.disconnect();
+  var statusSpan = document.getElementById("connectionStatus");
+  statusSpan.innerHTML = "Connection - Disconnected.";
+  connected = false;
+
+}
